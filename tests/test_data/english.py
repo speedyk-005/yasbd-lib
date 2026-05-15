fade86b4c311c02471912dff5983660f4d6d1011
speedyk-005 <speedy40115719@gmail.com>
2026-05-15 18:45:22 -0400

    test: use f-string for TOC leader test data

--- tests/test_data/english.py
+++ tests/test_data/english.py
@@ -52,7 +52,7 @@ TEST_DATA = [
     ("The project (Sinta) was nearing completion... or so we thought.", ["The project (Sinta) was nearing completion... or so we thought."]),
     ("\"How could we miss this!...\" Mark shouted, slamming his hand on the desk.", ["\"How could we miss this!...\"", "Mark shouted, slamming his hand on the desk."]),
     ("We found a memory leak in the C# wrapper.... It was subtle.", ["We found a memory leak in the C# wrapper....", "It was subtle."]),
-    ("Why Complex Carbohydrates.................................................. 19", ["Why Complex Carbohydrates.................................................. 19"]),
+    (f"Why Complex Carbohydrates{'.' *  15 } 19", [f"Why Complex Carbohydrates{'.' *  15 } 19"]),
 
     # Exclamation words
     ("We spent the afternoon playing Adopt Me! on the computer while eating Chips Ahoy! cookies.", ["We spent the afternoon playing Adopt Me! on the computer while eating Chips Ahoy! cookies."]),
