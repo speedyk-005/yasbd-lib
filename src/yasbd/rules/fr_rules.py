0c5df26683441026e5b50626c7fbcd6c9f36b483
speedyk-005 <speedy40115719@gmail.com>
2026-05-26 11:19:24 -0400

    feat(rules): add French (fr) language support

---
+++ src/yasbd/rules/fr_rules.py
@@ -0,0 +1,60 @@
+from yasbd.rules.base import Rules
+
+
+# fmt: off
+class FrRules(Rules):
+    ISO_CODE = "fr"
+
+    TITLE_ABBRVS = Rules.TITLE_ABBRVS | {
+        # Social / Professional
+        "m", "a.c.n", "a.m", "ch.-l", "e.v", "fr", "me", "mm", "r.p",
+
+        # Noble / Royal / Religious
+        "ll.aa", "ll.aa.ii", "ll.aa.rr", "ll.aa.ss", "ll.ee", "ll.mm",
+        "ll.mm.ii.rr", "nn.ss", "ll", "aa", "ii", "rr", "ss", "ee", "mm",
+    }
+
+    REFERENCE_ABBRVS = Rules.REFERENCE_ABBRVS | {
+        # Publishing / Documents
+        "ann", "chap", "coll", "dict", "fasc", "ill", "impr", "introd",
+        "ms", "pl", "pref", "suppl", "suiv", "t", "trad",
+    }
+
+    STREET_ABBRVS = Rules.STREET_ABBRVS | {
+        "av", "boul", "bd", "ch", "imp", "rte", "faub", "fg", "all", "carr",
+    }
+
+    MID_SENTENCE_ABBRVS = Rules.MID_SENTENCE_ABBRVS | {
+        "c.-à-d", "p.ex", "n.b", "p.s", "s.v.p",
+    }
+
+    DATE_ABBRVS = Rules.DATE_ABBRVS | {
+        # Months
+        "janv", "fevr", "févr", "fév", "avr", "juill", "juil", "sept",
+        "oct","nov", "déc",
+
+        # Days
+        "lun", "mar", "mer", "jeu", "ven", "sam", "dim",
+    }
+
+    COMMON_SENT_STARTERS = {
+        # Articles
+        "Le", "La", "Les", "L'",
+
+        # Pronouns
+        "Je", "Tu", "Il", "Elle", "On", "Nous", "Vous", "Ils", "Elles",
+        "Ce", "Cet", "Cette", "Ces",
+
+        # Adverbs
+        "Mais", "Donc", "Alors", "Cependant", "Toutefois", "Néanmoins",
+        "Ainsi", "Puis", "Ensuite", "Voici", "Voilà",
+
+        # Question words
+        "Qui", "Que", "Qu'", "Quand", "Où", "Pourquoi", "Comment", "Combien",
+        "Quel", "Quelle", "Quels", "Quelles",
+
+        # Other common starters
+        "Est-ce", "Millions",
+    }
+
+# fmt: on
