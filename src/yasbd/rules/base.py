9dbb4a7d080eb20e5ea6409123264cf7fb38e41d
speedyk-005 <speedy40115719@gmail.com>
2026-06-09 16:54:30 -0400

    fix(base): keep emojis attached to previous sentence (#73)

--- src/yasbd/rules/base.py
+++ src/yasbd/rules/base.py
@@ -195,21 +195,24 @@ class Rules:
         # https://regex101.com/r/VMzYsx/10
         cls.NAIVE_BOUNDARY_FINDER = re2.compile(
             rf"""
-            (?:
+            (?p)(?:
                 # Newline not followed by another newline
-                 (?<=\n(?!\n))|
+                 (?<=\n)(?!\n)|
 
                 # Split if left token is a unicase letter (Always)
                 (?<=\p{{Lo}}\s*[{cls.TERMINATORS_PATTERN}])|
 
                 # Split after any terminators followed by a a newline,
                 # common sentence starter, Space+Upper or unicase letter
-                (?<=[{cls.TERMINATORS_PATTERN}])
+                (?<=[{cls.TERMINATORS_PATTERN}](?!\s*\p{{Emoji_Presentation}}))
                 (?=
                     \s*[\n\p{{Lo}}]|
                     \s+(?:[^\p{{Ll}}]|
                     \s+(?<!\.\.)(?i:{cls.COMMON_STARTERS_PATTERN})\b)
                )|
+
+               # Emojis followed by an uppercase letter
+               (?<=\s*\p{{Emoji_Presentation}})(?=\s*\p{{Lu}})|
 
                 # Split at transition between Latin letters separate by alien
                 (?<=[\p{{LU}}\p{{Ll}}][​。！？।])(?=[\p{{Lu}}])
