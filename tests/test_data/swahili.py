ba773f73044580897b35cc38b5ad6f3e7aaf4244
speedyk-005 <speedy40115719@gmail.com>
2026-07-06 14:52:11 -0400

    test: add Swahili test data

---
+++ tests/test_data/swahili.py
@@ -0,0 +1,23 @@
+ISO_CODE = "sw"
+TEST_DATA = [
+    # Basic punctuation
+    "Habari za asubuhi.| Habari za jioni.",
+    "Unaitwa nani?| Ninaitwa Juma.",
+    "Hongera!| Umefanya vizuri.",
+
+    # Abbreviations
+    "Bw. Juma amefika.| Yuko ofisini.",
+    "Bi. Asha ni mwalimu.| Anafundisha shuleni.",
+    "Mh. Rais atazungumza kesho.| Wananchi wanasubiri.",
+    "Ndugu Wanjala alikaribishwa.| Alishukuru wote.",
+    "Eng. Njoroge anaendesha mradi.| Yeye ni mhandisi.",
+    "Mj. wa Kamati alitoa taarifa.| Kamati ilikubali.",
+    "Dkt. Mwangi atawasili kesho.| Ana mkutano saa tatu.",
+
+    # Parentheses and quotes
+    'Aliuliza "Unafanya nini?"| Nilijibu "Ninasoma."',
+    "Anakula matunda (maembe na ndizi).| Yeye ni hodari.",
+
+    # Ellipsis
+    "Alisema... sijui.| Kisha akaondoka.",
+]
